556d1f1bbf322acf7fdd9556cc12a84cc10ec2c2
Joshua Cold <joshua.cold@microfocus.com>
2021-01-09 16:16:21 -0700

    mimic family feud look using css. on elements

--- components/round.js
+++ components/round.js
@@ -4,41 +4,69 @@ export default function Round(props){
   let current_round = props.game.round
   let round = props.game.rounds[current_round]
   return (
-    <div >
-      <div class="text-center mt-5">
-        <p class="text-5xl p-7">ROUND {current_round +1}</p>
-        <p class="text-3xl">"{round.question}"</p>
-        <div class="rounded-lg border-4 w-max p-4 mt-10 mb-10 inline-block">
-          <p class="text-7xl">{props.game.point_tracker}</p>
+    <div class="">
+      <div class="text-center">
+        <div style={{borderWidth: 12}} class="border-black px-12 inline-block bg-gradient-to-tr
+          my-10 from-blue-900 to-blue-500">
+          <p class="text-white"style={{textShadow: "1px 2px 4px black", fontSize: 72, fontWeight: 600}}>{props.game.point_tracker}</p>
         </div>
+        <div class="my-5 flex flex-row justify-center">
+          <p class="text-end text-3xl ">{round.question}</p>
           {round.multiply > 1?
-            <p class="text-2xl">x{round.multiply}</p>:null
+            <div style={{transform: "translate(0px, -7px)"}}>
+            <p class="text-2xl text-start">x{round.multiply}</p>
+            </div>:null
           }
-      </div>
-
-      <div class="rounded border-4 grid grid-rows-4 grid-flow-col  p-3 mx-10 mb-10 mt-5 gap-3 bg-black">
-        {round.answers.map((x, index) =>
-          <div class="bg-blue-600 font-extrabold uppercase items-center text-center p-5 rounded border-2">
-            {x.trig?
-              <div class="flex">
-                <p class="text-xl text-white flex-grow">{x.ans}</p>
-                <p class="text-2xl text-white pr-5">{x.pnt}</p>
-              </div>
-            :<p class="text-3xl text-yellow-300">{index +1}</p>
-            }
+        </div>
+        <div class="flex flex-row items-center">
+          <div style={{borderWidth: 12}} class="border-black bg-gradient-to-tr px-12 ml-5
+            from-blue-900 to-blue-500 flex items-center justify-center">
+            <p class="text-white" style={{fontSize: 52, textShadow: "1px 2px 4px black"}}>{props.game.teams[0].points}</p>
           </div>
-        )}
+
+          <div class="rounded-3xl border-black grid grid-rows-4 grid-flow-col  p-3 mx-10 mb-10 gap-3 bg-black flex-grow"
+            style={{borderWidth: 12}}>
+            {round.answers.map((x, index) =>
+            <div class=" uppercase items-center text-center rounded border-2 font-extrabold">
+
+              {x.trig?
+                  // answered question
+                  <div class="h-full bg-gradient-to-t from-blue-800 via-blue-600 to-blue-800">
+                    <div class="flex h-full items-center justify-center">
+                      <p class="text-4xl text-white px-5 flex-grow" style={{textShadow: "1px 2px 4px black"}}>{x.ans}</p>
+                      <div class="bg-gradient-to-t from-blue-700 to-blue-400 border-l-2 h-full text-center items-center justify-center p-5 flex">
+                        <p class="text-4xl text-white"style={{textShadow: "1px 2px 4px black"}}>{x.pnt}</p>
+                      </div>
+                    </div>
+                  </div>
+                  :
+                  // unanswered question
+                  <div class="h-full bg-gradient-to-t py-3 flex justify-center items-center from-blue-700 to-blue-400 ">
+                    <div class="rounded-full justify-center items-center inline-block px-5 border-2 py-2 bg-gradient-to-tr
+                      from-blue-900 to-blue-700" style={{boxShadow: "3px 3px 2px black"}}>
+                      <p class="self-center text-5xl text-white font-black"style={{textShadow: "1px 2px 4px black"}}>{index +1}</p>
+                    </div>
+                  </div>
+              }
+            </div>
+            )}
+          </div>
+          <div style={{borderWidth: 12}} class="border-black bg-gradient-to-tr px-12 mr-5
+            from-blue-900 to-blue-500 flex items-center justify-center">
+            <p class="text-white" style={{fontSize: 52,textShadow: "1px 2px 4px black"}}>{props.game.teams[1].points}</p>
+          </div>
+        </div>
       </div>
-      <div class="text-3xl flex  px-6 text-center">
-        <div class="flex-grow">
-          <p class="pb-5">{props.game.teams[0].name}: {props.game.teams[0].points} </p>
+      <div class="text-3xl flex flex-row text-center justify-around mt-12">
+        <div class="bg-gradient-to-tr from-blue-900 to-blue-500">
+          <p class="p-5 uppercase text-white" style={{fontSize: 48, fontWeight:600,textShadow: "1px 2px 4px black"}}>{props.game.teams[0].name}</p>
           <div class="flex justify-center flex-row text-center">
             {Array(props.game.teams[0].mistakes).fill(<p>X</p>)}
           </div>
         </div>
-        <div class="flex-grow">
-          <p class="pb-5">{props.game.teams[1].name}: {props.game.teams[1].points}</p>
-          <div class="flex justify-center text-center flex-row flex-grow">
+        <div class="bg-gradient-to-tl from-blue-900 to-blue-500">
+          <p class="p-5 uppercase text-white" style={{fontSize: 48, fontWeight:600,textShadow: "1px 2px 4px black"}}>{props.game.teams[1].name}</p>
+          <div class="flex justify-center flex-row text-center">
             {Array(props.game.teams[1].mistakes).fill(<p>X</p>)}
           </div>
         </div>
