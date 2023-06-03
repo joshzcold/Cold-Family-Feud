65c4f8c1498fe955434c42cde6b3515bf311eeaf
Joshua Cold <joshua.cold@securitymetrics.com>
2023-06-03 01:32:21 -0600

    Fix serving of the logo images in production using a rest endpoint

--- server.js
+++ server.js
@@ -18,8 +18,16 @@ if (dev) {
 const PORT = process.env.PORT || 3000;
 app.prepare().then(async () => {
   createServer(httpsOptions, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    const logoRgex = /\/rooms\/([A-Z]{4})\/logo.(jpeg|gif|png)/
+    if(logoRgex.test(req.url)) {
+        const matched = req.url.match(logoRgex)
+        fs.readFile(`./public/rooms/${matched[1]}/logo.${matched[2]}`, function(err, data) {
+            res.end(data);
+        });
+    }else{
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }
   }).listen(PORT, (err) => {
     if (err) throw err;
     console.log(`> Ready on https://localhost:${PORT}`);
