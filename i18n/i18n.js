0148b2577cc80c1a90d7674ee651f45c62c0d68c
nickydotnet <contact@nicolas-denis.com>
2024-07-21 21:25:56 +0200

    french language declaration on i18n module

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -6,32 +6,36 @@ import translationEN from "public/locales/en/translation.json";
 import translationES from "public/locales/es/translation.json";
 import translationID from "public/locales/id/translation.json";
 import translationET from "public/locales/et/translation.json";
+import translationFR from "public/locales/fr/translation.json";
 
 const resources = {
-  en: {
-    translation: translationEN,
-  },
-  es: {
-    translation: translationES,
-  },
-  id: {
-    translation: translationID,
-  },
-  et: {
-    translation: translationET,
-  },
+    en: {
+        translation: translationEN,
+    },
+    es: {
+        translation: translationES,
+    },
+    id: {
+        translation: translationID,
+    },
+    et: {
+        translation: translationET,
+    },
+    fr: {
+        translation: translationFR,
+    },
 };
 i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    react: {
-      useSuspense: false,
-    },
-    fallbackLng: "en",
-    interpolation: {
-      format,
-    },
-    whitelist: ["es", "en", "id", "et"],
-  });
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        react: {
+            useSuspense: false,
+        },
+        fallbackLng: "en",
+        interpolation: {
+            format,
+        },
+        whitelist: ["es", "en", "id", "et", "fr"],
+    });
