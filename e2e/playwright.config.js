9bf36f56c156da56a44257dd890e34b346ec0ec6
Joshua Cold <joshua.cold@securitymetrics.com>
2024-12-08 16:23:47 -0700

    turn off parallel

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
