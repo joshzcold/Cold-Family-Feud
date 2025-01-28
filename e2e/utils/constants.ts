9694134c702459ed2f6002c2d8a80e661767763c
Karl Romet Somelar <karlromets@gmail.com>
2025-01-28 22:40:36 +0200

    chore(e2e): bring back /tmp/ dir for tests

--- e2e/utils/constants.ts
+++ e2e/utils/constants.ts
@@ -3,5 +3,5 @@ import path from "path";
 export const PATHS = {
   GAME_JSON: path.join(process.cwd(), "static/game.json"),
   GAME_CSV: path.join(process.cwd(), "static/game.csv"),
-  DOWNLOADED_GAME_JSON: path.join(process.cwd(), "static/downloaded-game.json"),
+  DOWNLOADED_GAME_JSON: path.join(process.cwd(), "/tmp/downloaded-game.json"),
 };
