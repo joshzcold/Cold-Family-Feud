053b1d4c952f78b1ded1f24ff3ffba2e502c2ed6
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-02-02 13:06:21 +0200

    test(e2e): optimize admin settings test setup
    - use `beforeAll` instead of `beforeEach`

--- e2e/tests/admin/settings.spec.ts
+++ e2e/tests/admin/settings.spec.ts
@@ -9,17 +9,18 @@ let host: Awaited<ReturnType<Setup["host"]>>;
 let adminPage: AdminPage;
 let spectator: Awaited<ReturnType<Setup["addPlayer"]>>;
 
-test.beforeEach(async ({ browser }) => {
+test.beforeAll(async ({ browser }) => {
   s = new Setup(browser);
   host = await s.host();
   adminPage = new AdminPage(host.page);
   spectator = await s.addPlayer(true);
+
+  await adminPage.gameSelector.selectOption({ index: 1 });
 });
 
 test("can edit title text", async ({ browser }: { browser: Browser }) => {
   const gamePage = new GamePage(spectator.page);
 
-  await adminPage.gameSelector.selectOption({ index: 1 });
   await adminPage.titleTextInput.fill("New Game Title");
   await expect(gamePage.titleLogoImg).toContainText("New Game Title");
 });
@@ -27,7 +28,6 @@ test("can edit title text", async ({ browser }: { browser: Browser }) => {
 test("can edit first team name text", async ({ browser }: { browser: Browser }) => {
   const gamePage = new GamePage(spectator.page);
 
-  await adminPage.gameSelector.selectOption({ index: 1 });
   await adminPage.teamOneNameInput.fill("Team Alpha");
   await expect(gamePage.getTeamNameByIndex(0)).toHaveText("Team Alpha");
 });
@@ -35,13 +35,11 @@ test("can edit first team name text", async ({ browser }: { browser: Browser }) 
 test("can edit second team name text", async ({ browser }: { browser: Browser }) => {
   const gamePage = new GamePage(spectator.page);
 
-  await adminPage.gameSelector.selectOption({ index: 1 });
   await adminPage.teamTwoNameInput.fill("Team Beta");
   await expect(gamePage.getTeamNameByIndex(1)).toHaveText("Team Beta");
 });
 
 test("can switch themes", async ({ browser }: { browser: Browser }) => {
-  await adminPage.gameSelector.selectOption({ index: 1 });
   const themeChanged = spectator.page.waitForFunction(() => document.body.classList.contains("darkTheme"), {
     timeout: 10000,
   });
@@ -53,7 +51,6 @@ test("can switch themes", async ({ browser }: { browser: Browser }) => {
 test("can hide questions", async ({ browser }: { browser: Browser }) => {
   const gamePage = new GamePage(spectator.page);
 
-  await adminPage.gameSelector.selectOption({ index: 1 });
   await adminPage.startRoundOneButton.click();
   await adminPage.hideQuestionsInput.click();
   await expect(gamePage.roundQuestionText).toBeVisible();
